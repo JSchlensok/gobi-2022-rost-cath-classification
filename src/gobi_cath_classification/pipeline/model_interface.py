e09bed3762e0560d492e5071e8717a246cc6e0c7
charlotte <chgerhaher@yahoo.com>
2022-02-07 19:27:20 +0100

    Move ModelInterface into model_interface.py

---
+++ src/gobi_cath_classification/pipeline/model_interface.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+from typing import List, Optional
+
+import numpy as np
+import pandas as pd
+
+
+class Prediction:
+    def __init__(self, probabilities: pd.DataFrame):
+        self.probabilities = probabilities
+        assert list(probabilities.columns) == sorted(probabilities.columns)
+        for col in probabilities.columns:
+            assert (
+                type(col) == str
+            ), f"Your column ({col}) should be a string, but it is of type: {type(col)}"
+            assert len(col.split(".")) == 4
+
+    def argmax_labels(self) -> List[str]:
+        y_pred_argmax_val = np.argmax(self.probabilities.values, axis=1)
+        y_pred_strings_val = [self.probabilities.columns[y] for y in y_pred_argmax_val]
+        return y_pred_strings_val
+
+
+class ModelInterface:
+    def train_one_epoch(
+        self,
+        embeddings: np.ndarray,
+        labels: List[str],
+        sample_weights: Optional[np.ndarray],
+    ) -> None:
+        """
+        Trains the model.
+
+        Args:
+            embeddings:
+                2D array with shape (number of embeddings, 1024)
+            labels:
+                2D array with shape (number of embeddings, number of classes) of
+                one-hot-encoded labels
+            sample_weights:
+                1D array with sample weights, shape (number of embeddings)
+
+        """
+        raise NotImplementedError
+
+    def predict_proba(self, embeddings: np.ndarray) -> Prediction:
+        """
+        Predicts probabilities for the CATH superfamily labels.
+
+        Args:
+            embeddings:
+                2D array with shape (number of embeddings, 1024)
+
+        Returns:
+            Pandas DataFrame with shape (number of embeddings, number of classes) of
+            probabilities. Each column corresponds to one CATH superfamily.
+        """
+        raise NotImplementedError
+
+    def save_checkpoint(self, save_to_dir: Path):
+        """
+
+        Save a checkpoint to given directory.
+
+        """
+        raise NotImplementedError
+
+    def load_model_from_checkpoint(self, load_from_dir: Path):
+        """
+
+        Load model from given checkpoint file(s),
+
+        """
+        raise NotImplementedError
--- src/gobi_cath_classification/scripts_charlotte/train_eval.py
+++ src/gobi_cath_classification/scripts_charlotte/train_eval.py
@@ -1,15 +1,11 @@
-from dataclasses import dataclass
-from pathlib import Path
 from typing import Optional, List, Dict
 
-import pandas as pd
 import ray
 from ray import tune
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, roc_auc_score
-from sklearn.naive_bayes import GaussianNB
 
+from gobi_cath_classification.pipeline.model_interface import Prediction
 from gobi_cath_classification.pipeline.sample_weights import (
     compute_inverse_sample_weights,
 )
@@ -17,139 +13,12 @@ from gobi_cath_classification.pipeline.data_loading import (
     load_data,
     check_if_cath_level_is_valid,
     label_for_level,
+    DATA_DIR,
 )
-
-
-class Prediction:
-    def __init__(self, probabilities: pd.DataFrame):
-        self.probabilities = probabilities
-        assert list(probabilities.columns) == sorted(probabilities.columns)
-        for col in probabilities.columns:
-            assert (
-                type(col) == str
-            ), f"Your column ({col}) should be a string, but it is of type: {type(col)}"
-            assert len(col.split(".")) == 4
-
-    def argmax_labels(self) -> List[str]:
-        y_pred_argmax_val = np.argmax(self.probabilities.values, axis=1)
-        y_pred_strings_val = [self.probabilities.columns[y] for y in y_pred_argmax_val]
-        return y_pred_strings_val
-
-
-class ModelInterface:
-    def train_one_epoch(
-        self,
-        embeddings: np.ndarray,
-        labels: List[str],
-        sample_weights: Optional[np.ndarray],
-    ) -> None:
-        """
-        Trains the model.
-
-        Args:
-            embeddings:
-                2D array with shape (number of embeddings, 1024)
-            labels:
-                2D array with shape (number of embeddings, number of classes) of
-                one-hot-encoded labels
-            sample_weights:
-                1D array with sample weights, shape (number of embeddings)
-
-        """
-        raise NotImplementedError
-
-    def predict_proba(self, embeddings: np.ndarray) -> Prediction:
-        """
-        Predicts probabilities for the CATH superfamily labels.
-
-        Args:
-            embeddings:
-                2D array with shape (number of embeddings, 1024)
-
-        Returns:
-            Pandas DataFrame with shape (number of embeddings, number of classes) of
-            probabilities. Each column corresponds to one CATH superfamily.
-        """
-        raise NotImplementedError
-
-    def save_checkpoint(self, save_to_dir: Path):
-        """
-
-        Save a checkpoint to given directory.
-
-        """
-        raise NotImplementedError
-
-    def load_model_from_checkpoint(self, load_from_dir: Path):
-        """
-
-        Load model from given checkpoint file(s),
-
-        """
-        raise NotImplementedError
-
-
-class RandomForestModel(ModelInterface):
-    def __init__(
-        self,
-        n_estimators=100,
-        max_depth=None,
-        bootstrap=True,
-        oob_score=False,
-        n_jobs=None,
-        class_weight=None,
-    ):
-        self.model = RandomForestClassifier(
-            n_estimators=n_estimators,
-            max_depth=max_depth,
-            bootstrap=bootstrap,
-            oob_score=oob_score,
-            n_jobs=n_jobs,
-            class_weight=class_weight,
-        )
-
-    def train_one_epoch(
-        self,
-        embeddings: np.ndarray,
-        labels: List[str],
-        sample_weights: Optional[np.ndarray],
-    ) -> None:
-        self.model.fit(X=embeddings, y=labels, sample_weight=sample_weights)
-
-    def predict_proba(self, embeddings: np.ndarray) -> Prediction:
-        predictions = self.model.predict_proba(X=embeddings)
-        df = pd.DataFrame(data=predictions, columns=self.model.classes_)
-        return Prediction(probabilities=df)
-
-    def save_checkpoint(self, save_to_dir: Path):
-        raise NotImplementedError
-
-    def load_model_from_checkpoint(self, load_from_dir: Path):
-        raise NotImplementedError
-
-
-class GaussianNaiveBayesModel(ModelInterface):
-    def __init__(self):
-        self.model = GaussianNB()
-
-    def train_one_epoch(
-        self,
-        embeddings: np.ndarray,
-        labels: List[str],
-        sample_weights: Optional[np.ndarray],
-    ) -> None:
-        self.model.fit(X=embeddings, y=labels, sample_weight=sample_weights)
-
-    def predict_proba(self, embeddings: np.ndarray) -> Prediction:
-        predictions = self.model.predict_proba(X=embeddings)
-        df = pd.DataFrame(data=predictions, columns=self.model.classes_)
-        return Prediction(probabilities=df)
-
-    def save_checkpoint(self, save_to_dir: Path):
-        raise NotImplementedError
-
-    def load_model_from_checkpoint(self, load_from_dir: Path):
-        raise NotImplementedError
+from gobi_cath_classification.scripts_charlotte.models import (
+    GaussianNaiveBayesModel,
+    RandomForestModel,
+)
 
 
 def accuracy_for_level(y_true: List[str], y_pred: List[str], cath_level: str) -> float:
@@ -186,11 +55,10 @@ def evaluate(y_true: List[str], y_pred: Prediction) -> Dict[str, float]:
 
 
 def training_function(config: dict):
-    # data
-    # TODO implement function that loads a small sample for quick test
+    data_dir = DATA_DIR
 
-    data_dir = Path(__file__).parent.parent.parent.parent / "data"
-    data_set = load_data(data_dir=data_dir, without_duplicates=True)
+    # TODO implement function that loads a small sample for quick test
+    data_set = load_data(data_dir=data_dir, without_duplicates=True, shuffle_data=True)
     num_classes = len(data_set.all_labels_train_sorted)
     print(f"num_classes = {num_classes}")
 
@@ -224,7 +92,7 @@ def training_function(config: dict):
         model.train_one_epoch(
             embeddings=data_set.X_train[:n, :],
             labels=data_set.y_train[:n],
-            sample_weights=sample_weights[:n],
+            sample_weights=sample_weights[:n] if sample_weights is not None else None,
         )
 
         print(f"Predicting for X_val with model {model.__class__.__name__}...")
@@ -232,12 +100,11 @@ def training_function(config: dict):
 
         # evaluate and save results in ray tune
         eval_dict = evaluate(y_true=data_set.y_val, y_pred=y_pred_val)
-
         tune.report(**eval_dict)
 
 
 def main():
-    # TODO ray tune checkpoint models ?
+    # TODO ray tune checkpoint models
     ray.init()
     analysis = tune.run(
         training_function,
