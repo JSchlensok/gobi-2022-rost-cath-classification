d9d30b28ba7fe9d71fa5d994761c2e086811aa30
charlotte <chgerhaher@yahoo.com>
2022-02-15 14:04:03 +0100

    Add set_random_seeds() to torch_utils.py

--- src/gobi_cath_classification/pipeline/torch_utils.py
+++ src/gobi_cath_classification/pipeline/torch_utils.py
@@ -1,6 +1,24 @@
+import os
+import random
+
+import numpy as np
 import torch
+
+RANDOM_SEED = 42
 
 
 def get_device():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     return device
+
+
+def set_random_seeds(seed=42):
+    np.random.seed(seed=seed)
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
