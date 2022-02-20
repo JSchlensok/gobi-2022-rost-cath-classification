15d571ad1654f78005a9e48ee061033e73dab9de
Julius Schlensok <julius@schlensok.org>
2022-02-20 23:12:28 +0100

    Implement test for data_loading

---
+++ src/gobi_cath_classification/pipeline/data/test_data_loading.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+
+from ..data import Dataset, load_data
+
+
+def test_loading():
+    dataset = load_data(Path(__file__).parent.parent.parent.parent.parent / "data", True, True, False)
+
