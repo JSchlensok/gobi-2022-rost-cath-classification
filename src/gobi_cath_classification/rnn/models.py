6f857760c37c5612654e4d147c86a8986f36e1ec
koenigj <koenigj@cip.ifi.lmu.de>
2022-02-23 21:37:30 +0100

    Erste LSTM

---
+++ src/gobi_cath_classification/rnn/models.py
@@ -0,0 +1,116 @@
+import math
+from pathlib import Path
+from typing import List, Optional, Dict
+
+import numpy as np
+import pandas as pd
+import torch
+from sklearn.preprocessing import OneHotEncoder
+from torch import nn
+from torch.nn.functional import one_hot
+
+from src.gobi_cath_classification.pipeline.model_interface import ModelInterface, Prediction
+from src.gobi_cath_classification.pipeline import torch_utils
+from src.gobi_cath_classification.pipeline.torch_utils import set_random_seeds
+
+
+class RNNModel(nn.Module):
+    def __init__(
+        self,
+        lr: float,
+        batch_size: int,
+        optimizer: str,
+        class_names: List[str],
+        hidden_dim: int,
+        num_layers: int,
+    ):
+        super(RNNModel, self).__init__()
+        self.device = torch_utils.get_device()
+
+        self.batch_size = batch_size
+        self.class_names = sorted(class_names)
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.categories = [
+            "A    R    N    D    C    Q    E    G    H    I    L    K    M    F    P    S    "
+            "T    W    Y    V    B    Z    X".split()
+        ]
+
+        self.lstm = nn.LSTM(23, hidden_dim, num_layers, batch_first=True).to(self.device)
+        self.fc = nn.Linear(in_features=hidden_dim, out_features=len(self.class_names)).to(
+            self.device
+        )
+
+        self.relu = nn.ReLU().to(self.device)
+        self.loss_function = torch.nn.CrossEntropyLoss(weight=None)
+
+        if optimizer == "sgd":
+            self.optimizer = torch.optim.SGD(self.parameters(), lr=lr)
+        elif optimizer == "adam":
+            self.optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+        else:
+            raise ValueError(f"Optimizer is not valid: {optimizer}")
+
+    def forward(self, x):
+        output, (hn, cn) = self.lstm(x)
+        hn = hn.view(-1, self.hidden_dim)
+        out = self.relu(hn)
+        out = self.fc(out)
+        out = self.relu(out)
+        return out
+
+    def one_hot_encode(self, X: List[str]):
+        max_length = np.max([len(seq) for seq in X])
+        X = [seq.ljust(max_length, "Q") for seq in X]  # Pad to the left
+        X = [list(seq) for seq in X]
+        X = np.array(X)
+        X = X.reshape(X.shape[0], -1, 1)
+        encoder = OneHotEncoder(categories=self.categories, handle_unknown="ignore")
+        X = np.array([encoder.fit_transform(x).toarray() for x in X])
+        return torch.tensor(X)
+
+    def train_one_epoch(
+        self,
+        sequences: List[str],
+        labels: List[str],
+        sample_weights: Optional[np.ndarray],
+    ) -> Dict[str, float]:
+
+        list_perm = np.random.permutation(len(sequences))
+        tensor_perm = torch.tensor(list_perm, dtype=torch.long)
+
+        y_indices = torch.tensor([self.class_names.index(label) for label in labels]).to(
+            self.device
+        )
+
+        y_one_hot = 1.0 * one_hot(y_indices, num_classes=len(self.class_names))
+        loss_sum = 0
+
+        for i in range(0, len(sequences), self.batch_size):
+            list_indices = list_perm[i : i + self.batch_size]
+            tensor_indices = tensor_perm[i : i + self.batch_size]
+            batch_X = [sequences[index] for index in list_indices]
+            # One Hot Encoding
+            batch_X = self.one_hot_encode(batch_X).to(self.device)
+            batch_y = y_one_hot[tensor_indices]
+            y_pred = self.forward(batch_X.float())
+            self.optimizer.zero_grad()
+
+            loss = self.loss_function(y_pred, batch_y)
+
+            loss_sum += loss
+            loss.backward()
+            self.optimizer.step()
+
+        loss_avg = float(loss_sum / (math.ceil(len(sequences) / self.batch_size)))
+        model_specific_metrics = {"loss_avg": loss_avg}
+        return model_specific_metrics
+
+    def predict(self, sequences: List[str]):
+        X = self.one_hot_encode(sequences).float()
+        predicted_probabilities = self.forward(X.to(self.device))
+        print(predicted_probabilities)
+        df = pd.DataFrame(
+            predicted_probabilities, columns=[str(label) for label in self.class_names]
+        ).astype(float)
+        return Prediction(probabilities=df)
---
+++ src/gobi_cath_classification/rnn/train_rnn.py
@@ -0,0 +1,130 @@
+import ray
+import torch
+import numpy as np
+from ray import tune
+
+from src.gobi_cath_classification.pipeline import torch_utils
+from src.gobi_cath_classification.pipeline.torch_utils import RANDOM_SEED
+from src.gobi_cath_classification.pipeline.evaluation import evaluate
+from src.gobi_cath_classification.rnn.models import (
+    RNNModel,
+)
+from src.gobi_cath_classification.rnn.pipeline import DATA_DIR, load_data
+
+
+def training_function(config: dict) -> None:
+    # set random seeds
+    random_seed = config["random_seed"]
+    rng = np.random.RandomState(random_seed)
+    print(f"rng = {rng}")
+
+    # load data
+    data_dir = DATA_DIR
+    data_set = load_data(data_dir=data_dir, without_duplicates=True, shuffle_data=True, rng=rng)
+    sequences_train = data_set.X_train
+
+    y_train_labels = data_set.y_train
+
+    class_names = data_set.all_labels_train_sorted
+    print(f"len(class_names) = {len(class_names)}")
+
+    # get hyperparameters from config dict
+    print(f"config = {config}")
+    num_epochs = config["model"]["num_epochs"]
+    model_class = config["model"]["model_class"]
+
+    if config["class_weights"] == "none":
+        sample_weights = None
+        class_weights = None
+    else:
+        raise ValueError(f'Class weights do not exist: {config["class_weights"]}')
+
+    # set model
+    if model_class == RNNModel.__name__:
+        model = RNNModel(
+            lr=config["model"]["lr"],
+            class_names=class_names,
+            batch_size=config["model"]["batch_size"],
+            optimizer=config["model"]["optimizer"],
+            hidden_dim=config["model"]["hidden_dim"],
+            num_layers=config["model"]["num_layers"],
+        )
+    else:
+        raise ValueError(f"Model class {model_class} does not exist.")
+
+    # set variables for early stopping
+    highest_acc_h = 0
+    n_bad = 0
+    n_thresh = 20
+
+    print(f"Training model {model.__class__.__name__}...")
+    for epoch in range(num_epochs):
+        model_metrics_dict = model.train_one_epoch(
+            sequences=sequences_train,
+            labels=y_train_labels,
+            sample_weights=None,
+        )
+
+        print(f"Predicting for X_val with model {model.__class__.__name__}...")
+        y_pred_val = model.predict(data_set.X_val)
+
+        # evaluate and save results in ray tune
+        eval_dict = evaluate(
+            y_true=data_set.y_val,
+            y_pred=y_pred_val,
+            class_names_training=data_set.all_labels_train_sorted,
+        )
+        tune.report(**eval_dict, **{f"model_{k}": v for k, v in model_metrics_dict.items()})
+
+        # check for early stopping
+        acc_h = eval_dict["accuracy_h"]
+        if acc_h > highest_acc_h:
+            highest_acc_h = acc_h
+            n_bad = 0
+            print(f"New best performance found: accuracy_h = {highest_acc_h}")
+        else:
+            n_bad += 1
+            if n_bad >= n_thresh:
+                break
+
+
+def main():
+    print(f"torch.cuda.is_available() = {torch.cuda.is_available()}")
+    device = torch_utils.get_device()
+    print(f"device = {device}")
+
+    if torch.cuda.is_available():
+        resources_per_trial = {"gpu": 1}
+    else:
+        resources_per_trial = {"cpu": 1}
+
+    reporter = tune.CLIReporter(
+        max_report_frequency=10,
+        infer_limit=10,
+    )
+
+    ray.init()
+    analysis = tune.run(
+        training_function,
+        resources_per_trial=resources_per_trial,
+        num_samples=1,
+        config={
+            "random_seed": tune.grid_search([1]),
+            "class_weights": tune.grid_search(["none"]),
+            "model": {
+                "model_class": RNNModel.__name__,
+                "num_epochs": 10,
+                "lr": tune.grid_search([1e-5, 1e-4, 1e-3]),
+                "batch_size": 100,
+                "optimizer": tune.choice(["adam"]),
+                "hidden_dim": tune.choice([5, 20]),
+                "num_layers": 1,
+            },
+        },
+        progress_reporter=reporter,
+    )
+    print("Best config: ", analysis.get_best_config(metric="accuracy_h", mode="max"))
+
+
+if __name__ == "__main__":
+    main()
