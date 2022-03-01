759006a6ae52c2c749979d87a61edd6f7d488fc6
koenigj <koenigj@cip.ifi.lmu.de>
2022-03-01 14:19:08 +0100

    Last commit

--- src/gobi_cath_classification/rnn/models.py
+++ src/gobi_cath_classification/rnn/models.py
@@ -14,6 +14,20 @@ from gobi_cath_classification.pipeline.model_interface import ModelInterface, Pr
 from gobi_cath_classification.pipeline import torch_utils
 from gobi_cath_classification.pipeline.torch_utils import set_random_seeds
 
+categories = ["A    R    N    D    C    Q    E    G    H    I    L    K    M    F    P    S    "
+              "T    W    Y    V    B    Z    X".split()]
+
+
+def one_hot_encode(X: List[str]):
+    max_length = np.max([len(seq) for seq in X])
+    X = [seq.ljust(max_length, "Q") for seq in X]  # Pad to the left
+    X = [list(seq) for seq in X]
+    X = np.array(X)
+    X = X.reshape(X.shape[0], -1, 1)
+    encoder = OneHotEncoder(categories=categories, handle_unknown="ignore")
+    X = np.array([encoder.fit_transform(x).toarray() for x in X])
+    return Variable(torch.tensor(X))
+
 
 class RNNModel(nn.Module):
     def __init__(
@@ -32,10 +46,6 @@ class RNNModel(nn.Module):
         self.class_names = sorted(class_names)
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
-        self.categories = [
-            "A    R    N    D    C    Q    E    G    H    I    L    K    M    F    P    S    "
-            "T    W    Y    V    B    Z    X".split()
-        ]
 
         self.lstm = nn.LSTM(
             input_size=23,
@@ -58,7 +68,7 @@ class RNNModel(nn.Module):
         else:
             raise ValueError(f"Optimizer is not valid: {optimizer}")
 
-    def forward(self, x: torch.tensor):
+    def forward(self, x):
         # print(f"Input = {x.size()}")
         h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_dim)).to(self.device)
         c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_dim)).to(self.device)
@@ -72,16 +82,6 @@ class RNNModel(nn.Module):
         # print(f"Fully connected = {out.size()}")
         out = self.sigmoid(out)
         return out
-
-    def one_hot_encode(self, X: List[str]):
-        max_length = np.max([len(seq) for seq in X])
-        X = [seq.ljust(max_length, "Q") for seq in X]  # Pad to the left
-        X = [list(seq) for seq in X]
-        X = np.array(X)
-        X = X.reshape(X.shape[0], -1, 1)
-        encoder = OneHotEncoder(categories=self.categories, handle_unknown="ignore")
-        X = np.array([encoder.fit_transform(x).toarray() for x in X])
-        return Variable(torch.tensor(X))
 
     def train_one_epoch(
             self,
@@ -105,7 +105,7 @@ class RNNModel(nn.Module):
             tensor_indices = tensor_perm[i:i + self.batch_size]
             batch_X = [sequences[index] for index in list_indices]
             # One Hot Encoding
-            batch_X = self.one_hot_encode(batch_X).to(self.device)
+            batch_X = one_hot_encode(batch_X).to(self.device)
             batch_y = Variable(y_one_hot[tensor_indices])
             self.optimizer.zero_grad()
             y_pred = self.forward(batch_X.float())
@@ -128,3 +128,53 @@ class RNNModel(nn.Module):
             predicted_probabilities, columns=[str(label) for label in self.class_names]
         ).astype(float)
         return Prediction(probabilities=df)
+
+
+class LSTMTagger(nn.Module):
+    def __init__(self, hidden_dim: int, class_names: List[str], categories: int = 23):
+        super(LSTMTagger, self).__init__()
+
+        self.hidden_dim = hidden_dim
+        self.lstm = nn.LSTM(categories, hidden_dim, batch_first=True, bidirectional=True)
+        self.softmax = nn.Softmax(dim=1)
+
+        self.fc = nn.Linear(hidden_dim, len(class_names))
+
+    def forward(self, x):
+        lstm_out, _ = self.lstm(x.view(x.size(0), x.size(1), -1))
+        print(f"lstm_out: {lstm_out.size()}")
+        fc_out = self.fc(lstm_out.view(x.size(0), x.size(1), -1))
+        fc_scores = self.softmax(fc_out)
+        print(fc_scores.size())
+        return fc_scores
+
+
+class BRNN(nn.Module):
+    def __init__(self, hidden_size, num_layers, num_classes, input_size=23, lr=0.01, batch_size=300):
+        super(BRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers  #
+        self.device = torch_utils.get_device()
+
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers,
+                            bidirectional=True, batch_first=True).to(self.device)
+        self.fc = nn.Linear(hidden_size * 2, num_classes).to(self.device)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).to(self.device)
+        c0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).to(self.device)
+
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        out = self.sigmoid(out)
+
+        return out
+
+    def train_one_epoch(self, sequences:List[str], labels:List[str]) -> Dict[str, float]:
+        list_perm = np.random.permutation(len(sequences))
+        tensor_perm = torch.tensor(list_perm, dtype=torch.long)
+
+        y_indices = torch.tensor([self.class_names.index(label) for label in labels]).to(
+            self.device
+        )
