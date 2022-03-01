4e6555a2194d3497e0c1614762398dad2616eb5f
koenigj <koenigj@cip.ifi.lmu.de>
2022-03-01 17:38:26 +0100

    Neue Modelle

--- src/gobi_cath_classification/rnn/models.py
+++ src/gobi_cath_classification/rnn/models.py
@@ -7,6 +7,7 @@ import pandas as pd
 import torch
 from sklearn.preprocessing import OneHotEncoder
 from torch import nn
+from torch.autograd import Variable
 from torch.nn.functional import one_hot
 
 from gobi_cath_classification.pipeline.model_interface import ModelInterface, Prediction
@@ -25,18 +26,19 @@ def one_hot_encode(X: List[str]):
     X = X.reshape(X.shape[0], -1, 1)
     encoder = OneHotEncoder(categories=categories, handle_unknown="ignore")
     X = np.array([encoder.fit_transform(x).toarray() for x in X])
+    print(X.tolist())
     return Variable(torch.tensor(X))
 
 
 class RNNModel(nn.Module):
     def __init__(
-        self,
-        lr: float,
-        batch_size: int,
-        optimizer: str,
-        class_names: List[str],
-        hidden_dim: int,
-        num_layers: int,
+            self,
+            lr: float,
+            batch_size: int,
+            optimizer: str,
+            class_names: List[str],
+            hidden_dim: int,
+            num_layers: int,
     ):
         super(RNNModel, self).__init__()
         self.device = torch_utils.get_device()
@@ -46,12 +48,18 @@ class RNNModel(nn.Module):
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
 
-        self.lstm = nn.LSTM(23, hidden_dim, num_layers, batch_first=True).to(self.device)
+        self.lstm = nn.LSTM(
+            input_size=23,
+            hidden_size=hidden_dim,
+            num_layers=num_layers,
+            batch_first=True
+        ).to(self.device)
         self.fc = nn.Linear(in_features=hidden_dim, out_features=len(self.class_names)).to(
             self.device
         )
 
         self.relu = nn.ReLU().to(self.device)
+        self.sigmoid = nn.Sigmoid()
         self.loss_function = torch.nn.CrossEntropyLoss(weight=None)
 
         if optimizer == "sgd":
@@ -62,40 +70,25 @@ class RNNModel(nn.Module):
             raise ValueError(f"Optimizer is not valid: {optimizer}")
 
     def forward(self, x):
-<<<<<<< HEAD
         # print(f"Input = {x.size()}")
         h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_dim)).to(self.device)
         c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_dim)).to(self.device)
         output, (hn, cn) = self.lstm(x, (h_0, c_0))
         # print(f"Output = {output.size()}")
         # print(f"Hidden = {hn.size()}")
-=======
-        output, (hn, cn) = self.lstm(x)
->>>>>>> f418a182f6a7164a0911336be0ab0f800dfdc34f
         hn = hn.view(-1, self.hidden_dim)
+        # print(f"Hidden2 = {hn.size()}")
         out = self.relu(hn)
         out = self.fc(out)
-        out = self.relu(out)
+        # print(f"Fully connected = {out.size()}")
+        out = self.sigmoid(out)
         return out
 
-<<<<<<< HEAD
-=======
-    def one_hot_encode(self, X: List[str]):
-        max_length = np.max([len(seq) for seq in X])
-        X = [seq.ljust(max_length, "Q") for seq in X]  # Pad to the left
-        X = [list(seq) for seq in X]
-        X = np.array(X)
-        X = X.reshape(X.shape[0], -1, 1)
-        encoder = OneHotEncoder(categories=self.categories, handle_unknown="ignore")
-        X = np.array([encoder.fit_transform(x).toarray() for x in X])
-        return torch.tensor(X)
-
->>>>>>> f418a182f6a7164a0911336be0ab0f800dfdc34f
     def train_one_epoch(
-        self,
-        sequences: List[str],
-        labels: List[str],
-        sample_weights: Optional[np.ndarray],
+            self,
+            sequences: List[str],
+            labels: List[str],
+            sample_weights: Optional[np.ndarray],
     ) -> Dict[str, float]:
 
         list_perm = np.random.permutation(len(sequences))
@@ -109,20 +102,14 @@ class RNNModel(nn.Module):
         loss_sum = 0
 
         for i in range(0, len(sequences), self.batch_size):
-            list_indices = list_perm[i : i + self.batch_size]
-            tensor_indices = tensor_perm[i : i + self.batch_size]
+            list_indices = list_perm[i:i + self.batch_size]
+            tensor_indices = tensor_perm[i:i + self.batch_size]
             batch_X = [sequences[index] for index in list_indices]
             # One Hot Encoding
-<<<<<<< HEAD
             batch_X = one_hot_encode(batch_X).to(self.device)
             batch_y = Variable(y_one_hot[tensor_indices])
             self.optimizer.zero_grad()
-=======
-            batch_X = self.one_hot_encode(batch_X).to(self.device)
-            batch_y = y_one_hot[tensor_indices]
->>>>>>> f418a182f6a7164a0911336be0ab0f800dfdc34f
             y_pred = self.forward(batch_X.float())
-            self.optimizer.zero_grad()
 
             loss = self.loss_function(y_pred, batch_y)
 
@@ -164,31 +151,67 @@ class LSTMTagger(nn.Module):
 
 
 class BRNN(nn.Module):
-    def __init__(self, hidden_size, num_layers, num_classes, input_size=23, lr=0.01, batch_size=300):
+    def __init__(self,
+                 hidden_size,
+                 num_layers,
+                 class_names,
+                 input_size=23,
+                 class_weights=None,
+                 lr=0.01,
+                 batch_size=300):
         super(BRNN, self).__init__()
         self.hidden_size = hidden_size
-        self.num_layers = num_layers  #
+        self.num_layers = num_layers
         self.device = torch_utils.get_device()
+        self.batch_size = batch_size
+        self.class_names = class_names
 
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers,
                             bidirectional=True, batch_first=True).to(self.device)
-        self.fc = nn.Linear(hidden_size * 2, num_classes).to(self.device)
+        self.fc = nn.Linear(hidden_size * 2, len(class_names)).to(self.device)
         self.sigmoid = nn.Sigmoid()
+
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+        self.loss_function = torch.nn.CrossEntropyLoss(
+            weight=class_weights.to(self.device) if class_weights is not None else None,
+        )
 
     def forward(self, x):
         h0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).to(self.device)
         c0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).to(self.device)
-
         out, _ = self.lstm(x, (h0, c0))
         out = self.fc(out[:, -1, :])
         out = self.sigmoid(out)
 
         return out
 
-    def train_one_epoch(self, sequences:List[str], labels:List[str]) -> Dict[str, float]:
+    def train_one_epoch(self, sequences: List[str], labels: List[str]) -> Dict[str, float]:
         list_perm = np.random.permutation(len(sequences))
         tensor_perm = torch.tensor(list_perm, dtype=torch.long)
 
         y_indices = torch.tensor([self.class_names.index(label) for label in labels]).to(
             self.device
         )
+
+        y_one_hot = 1.0 * one_hot(y_indices, num_classes=len(self.class_names))
+        loss_sum = 0
+
+        for i in range(0, len(sequences), self.batch_size):
+            list_indices = list_perm[i:i + self.batch_size]
+            tensor_indices = tensor_perm[i:i + self.batch_size]
+            batch_X = [sequences[index] for index in list_indices]
+            # One Hot Encoding
+            batch_X = one_hot_encode(batch_X).to(self.device)
+            batch_y = Variable(y_one_hot[tensor_indices])
+            self.optimizer.zero_grad()
+            y_pred = self.forward(batch_X.float())
+
+            loss = self.loss_function(y_pred, batch_y)
+
+            loss_sum += loss
+            loss.backward()
+            self.optimizer.step()
+
+        loss_avg = float(loss_sum / (math.ceil(len(sequences) / self.batch_size)))
+        model_specific_metrics = {"loss_avg": loss_avg}
+        return model_specific_metrics
---
+++ src/gobi_cath_classification/rnn/test_models.py
@@ -0,0 +1,39 @@
+import math
+import pickle
+from pathlib import Path
+from typing import List, Optional, Dict
+
+import numpy as np
+import pandas as pd
+import torch
+from gobi_cath_classification.pipeline.sample_weights import compute_inverse_sample_weights, compute_class_weights
+from torch import nn, optim
+from torch.nn.functional import one_hot
+from sklearn.preprocessing import OneHotEncoder
+
+from gobi_cath_classification.pipeline.model_interface import ModelInterface, Prediction
+from gobi_cath_classification.pipeline import torch_utils
+from gobi_cath_classification.pipeline.torch_utils import set_random_seeds
+from gobi_cath_classification.rnn.models import RNNModel, LSTMTagger, BRNN, one_hot_encode
+from gobi_cath_classification.pipeline.data.data_loading import load_data
+from gobi_cath_classification.pipeline.data_loading import DATA_DIR
+from gobi_cath_classification.pipeline.data.Dataset import Dataset
+
+dataset = pickle.load(
+    open(DATA_DIR / "serialized_dataset_no-duplicates_full_with_strings.pickle", 'rb')
+)
+# dataset = load_data(
+# DATA_DIR, np.random.RandomState(42), without_duplicates=True, load_strings=True
+# )
+sample_weights = compute_inverse_sample_weights(labels=dataset.y_train)
+class_weights = compute_class_weights(labels=dataset.y_train)
+class_names = dataset.train_labels
+
+model = BRNN(hidden_size=128, num_layers=1, class_names=class_names)
+X_train, y_train_labels = dataset.get_split("train", x_encoding='string', zipped=False)
+
+for e in range(50):
+    metrics = model.train_one_epoch(X_train, y_train_labels)
+    print(f"Epoch {e}")
+    print(f"Avg Loss{metrics['loss_avg']}")
+
