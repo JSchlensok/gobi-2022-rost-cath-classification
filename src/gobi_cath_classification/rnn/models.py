4ec7c546076e8b2d3684a3b2b1ccca18432d193e
koenigj <koenigj@cip.ifi.lmu.de>
2022-03-02 22:37:05 +0100

    Progress report

--- src/gobi_cath_classification/rnn/models.py
+++ src/gobi_cath_classification/rnn/models.py
@@ -18,13 +18,13 @@ from gobi_cath_classification.rnn.encoder import one_hot_encode, num_categories
 
 class RNNModel(nn.Module):
     def __init__(
-        self,
-        lr: float,
-        batch_size: int,
-        optimizer: str,
-        class_names: List[str],
-        hidden_dim: int,
-        num_layers: int,
+            self,
+            lr: float,
+            batch_size: int,
+            optimizer: str,
+            class_names: List[str],
+            hidden_dim: int,
+            num_layers: int,
     ):
         super(RNNModel, self).__init__()
         self.device = torch_utils.get_device()
@@ -71,10 +71,10 @@ class RNNModel(nn.Module):
         return out
 
     def train_one_epoch(
-        self,
-        sequences: List[str],
-        labels: List[str],
-        sample_weights: Optional[np.ndarray],
+            self,
+            sequences: List[str],
+            labels: List[str],
+            sample_weights: Optional[np.ndarray],
     ) -> Dict[str, float]:
 
         list_perm = np.random.permutation(len(sequences))
@@ -88,8 +88,8 @@ class RNNModel(nn.Module):
         loss_sum = 0
 
         for i in range(0, len(sequences), self.batch_size):
-            list_indices = list_perm[i : i + self.batch_size]
-            tensor_indices = tensor_perm[i : i + self.batch_size]
+            list_indices = list_perm[i: i + self.batch_size]
+            tensor_indices = tensor_perm[i: i + self.batch_size]
             batch_X = [sequences[index] for index in list_indices]
             # One Hot Encoding
             batch_X = one_hot_encode(batch_X).to(self.device)
@@ -117,34 +117,15 @@ class RNNModel(nn.Module):
         return Prediction(probabilities=df)
 
 
-class LSTMTagger(nn.Module):
-    def __init__(self, hidden_dim: int, class_names: List[str]):
-        super(LSTMTagger, self).__init__()
-
-        self.hidden_dim = hidden_dim
-        self.lstm = nn.LSTM(num_categories, hidden_dim, batch_first=True, bidirectional=True)
-        self.softmax = nn.Softmax(dim=1)
-
-        self.fc = nn.Linear(hidden_dim, len(class_names))
-
-    def forward(self, x):
-        lstm_out, _ = self.lstm(x.view(x.size(0), x.size(1), -1))
-        print(f"lstm_out: {lstm_out.size()}")
-        fc_out = self.fc(lstm_out.view(x.size(0), x.size(1), -1))
-        fc_scores = self.softmax(fc_out)
-        print(fc_scores.size())
-        return fc_scores
-
-
 class BRNN(nn.Module):
     def __init__(
-        self,
-        hidden_size,
-        num_layers,
-        class_names,
-        class_weights=None,
-        lr=0.01,
-        batch_size=200,
+            self,
+            hidden_size,
+            num_layers,
+            class_names,
+            class_weights=None,
+            lr=0.01,
+            batch_size=200,
     ):
         super(BRNN, self).__init__()
         self.hidden_size = hidden_size
@@ -169,14 +150,17 @@ class BRNN(nn.Module):
         h0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).to(self.device)
         c0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).to(self.device)
         out, _ = self.lstm(x, (h0, c0))
-        out_forward = out[:, -1, : self.hidden_size]
-        out_backward = out[:, 0, self.hidden_size :]
+        out_forward = out[:, -1, :self.hidden_size]
+        out_backward = out[:, 0, self.hidden_size:]
         out = self.fc(torch.cat((out_forward, out_backward), dim=1))
         out = self.softmax(out)
 
         return out
 
-    def train_one_epoch(self, sequences: List[str], labels: List[str]) -> Dict[str, float]:
+    def train_one_epoch(self,
+                        sequences: List[str],
+                        labels: List[str], report_progress=False
+                        ) -> Dict[str, float]:
         list_perm = np.random.permutation(len(sequences))
         tensor_perm = torch.tensor(list_perm, dtype=torch.long)
 
@@ -185,11 +169,12 @@ class BRNN(nn.Module):
         )
 
         y_one_hot = 1.0 * one_hot(y_indices, num_classes=len(self.class_names))
+        counter = 0
         loss_sum = 0
 
         for i in range(0, len(sequences), self.batch_size):
-            list_indices = list_perm[i : i + self.batch_size]
-            tensor_indices = tensor_perm[i : i + self.batch_size]
+            list_indices = list_perm[i:i + self.batch_size]
+            tensor_indices = tensor_perm[i:i + self.batch_size]
             batch_X = [sequences[index] for index in list_indices]
             # One Hot Encoding
             batch_X = one_hot_encode(batch_X).to(self.device)
@@ -202,6 +187,9 @@ class BRNN(nn.Module):
             loss_sum += loss
             loss.backward()
             self.optimizer.step()
+            if report_progress and (counter % 18 == 0):
+                print(f"\t\t{i + self.batch_size}/{len(sequences)} done")
+                counter += 1
 
         print(y_pred)
         loss_avg = float(loss_sum / (math.ceil(len(sequences) / self.batch_size)))
--- src/gobi_cath_classification/rnn/test_models.py
+++ src/gobi_cath_classification/rnn/test_models.py
@@ -17,7 +17,7 @@ from sklearn.preprocessing import OneHotEncoder
 from gobi_cath_classification.pipeline.model_interface import ModelInterface, Prediction
 from gobi_cath_classification.pipeline import torch_utils
 from gobi_cath_classification.pipeline.torch_utils import set_random_seeds
-from gobi_cath_classification.rnn.models import RNNModel, LSTMTagger, BRNN, one_hot_encode
+from gobi_cath_classification.rnn.models import RNNModel, BRNN, one_hot_encode
 from gobi_cath_classification.pipeline.data.data_loading import load_data
 from gobi_cath_classification.pipeline.data_loading import DATA_DIR
 from gobi_cath_classification.pipeline.data.Dataset import Dataset
@@ -46,14 +46,23 @@ sample_weights = compute_inverse_sample_weights(labels=dataset.y_train)
 class_weights = torch.tensor(compute_class_weights(labels=dataset.y_train))
 class_names = dataset.train_labels
 
-model = BRNN(hidden_size=512, num_layers=1, class_names=class_names, class_weights=class_weights, lr=1e-3, batch_size=32)
+model = BRNN(
+    hidden_size=256,
+    num_layers=1,
+    class_names=class_names,
+    class_weights=class_weights,
+    lr=1e-5,
+    batch_size=32,
+)
 X_train, y_train_labels = dataset.get_split("train", x_encoding="string", zipped=False)
 
 
 for e in range(100):
-    metrics = model.train_one_epoch(X_train, y_train_labels)
+    metrics = model.train_one_epoch(X_train, y_train_labels, report_progress=True)
     print(f"Epoch {e + 1}")
     print(f"Avg Loss {metrics['loss_avg']}")
     print(metrics)
+    if (e % 9) == 0:
+        torch.save(model, (DATA_DIR / "brnn.pth"))
 
 torch.save(model, (DATA_DIR / "brnn.pth"))
