ec8bc68574070fb94504d2b10ef8b601b8f1b319
anna-charlotte <chgerhaher@yahoo.com>
2022-03-11 14:15:51 +0100

    Fix path in model_interface_test.py

--- src/gobi_cath_classification/pipeline/model_interface_test.py
+++ src/gobi_cath_classification/pipeline/model_interface_test.py
@@ -1,3 +1,5 @@
+import os
+
 from pathlib import Path
 
 import numpy as np
@@ -11,7 +13,8 @@ from gobi_cath_classification.pipeline.model_interface import (
 
 
 def test_save_and_read_in_prediction():
-    filepath = Path(__file__).parent.parent.parent.parent / "predictions"
+    filepath = Path(__file__).parent.parent.parent.parent
+    filename = "pred_test.csv"
     pred_1 = Prediction(
         pd.DataFrame(
             data=np.array(
@@ -24,9 +27,8 @@ def test_save_and_read_in_prediction():
             columns=["1.20.35.10", "2.40.50.10", "3.20.25.400", "3.300.20.5"],
         )
     )
-    # ids_1 = ["id1", "id2", "id3"]
-    save_predictions(pred=pred_1, directory=filepath, filename="pred_test.csv")
+    save_predictions(pred=pred_1, directory=filepath, filename=filename)
     pred_2 = read_in_predictions(filepath=filepath / "pred_test.csv")
+    os.remove(path=filepath / filename)
 
     pd.testing.assert_frame_equal(pred_1.probabilities, pred_2.probabilities)
-    # assert ids_1 == ids_2
