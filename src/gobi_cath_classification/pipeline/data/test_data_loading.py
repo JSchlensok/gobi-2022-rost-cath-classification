8de0109941f9ba1f637b1fa94730538e0b0cf0e1
anna-charlotte <chgerhaher@yahoo.com>
2022-03-13 11:04:37 +0100

    Clean up test_data_loading

--- src/gobi_cath_classification/pipeline/data/test_data_loading.py
+++ src/gobi_cath_classification/pipeline/data/test_data_loading.py
@@ -1,39 +1,38 @@
 import numpy as np
 from pathlib import Path
 
-from ..data import Dataset, load_data
+from ..data import load_data
 
 
 def test_loading_with_shuffling():
     dataset = load_data(
-        Path(__file__).parent.parent.parent.parent.parent / "data",
-        np.random.RandomState(42),
-        True,
-        True,
-        False,
-        True,
+        data_dir=Path(__file__).parent.parent.parent.parent.parent / "data",
+        rng=np.random.RandomState(42),
+        without_duplicates=True,
+        shuffle_data=True,
+        load_only_small_sample=False,
+        reloading_allowed=True,
     )
 
 
 def test_loading_without_shuffling():
     dataset = load_data(
-        Path(__file__).parent.parent.parent.parent.parent / "data",
-        np.random.RandomState(42),
-        True,
-        True,
-        False,
-        True,
+        data_dir=Path(__file__).parent.parent.parent.parent.parent / "data",
+        rng=np.random.RandomState(42),
+        without_duplicates=True,
+        shuffle_data=True,
+        load_only_small_sample=False,
+        reloading_allowed=True,
     )
 
 
 def test_loading_with_strings():
-
     dataset = load_data(
-        Path(__file__).parent.parent.parent.parent.parent / "data",
-        np.random.RandomState(42),
-        True,
-        True,
-        False,
-        True,
-        True,
+        data_dir=Path(__file__).parent.parent.parent.parent.parent / "data",
+        rng=np.random.RandomState(42),
+        without_duplicates=True,
+        shuffle_data=True,
+        load_only_small_sample=False,
+        reloading_allowed=True,
+        load_strings=True,
     )
