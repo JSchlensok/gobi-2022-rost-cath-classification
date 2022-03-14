cbbdd145d3429708bf081d8675bece68c7734cb4
koenigj <koenigj@cip.ifi.lmu.de>
2022-03-15 00:26:17 +0100

    Neue embeddings testen

--- src/gobi_cath_classification/rnn/pipeline.py
+++ src/gobi_cath_classification/rnn/pipeline.py
@@ -1,5 +1,6 @@
 from collections import Counter
 from pathlib import Path
+from gobi_cath_classification.pipeline.data.data_loading import read_in_embeddings
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 from torch.nn.functional import one_hot
@@ -7,3 +8,7 @@ from torch.nn.functional import one_hot
 REPO_ROOT_DIR = Path(__file__).parent.parent.parent.parent.absolute()
 DATA_DIR = REPO_ROOT_DIR / "data"
 
+emb = read_in_embeddings(DATA_DIR / "t5_xl_v3_half_cath_S100_RESIDUE.h5")
+print(f"Länge: {len(emb)}")
+print(emb.keys()[0])
+print(emb.values()[0].shape)
