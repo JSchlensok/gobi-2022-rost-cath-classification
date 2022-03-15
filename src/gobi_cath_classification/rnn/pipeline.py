1b4e2877fb6c37a458dd3402a12c5d1b5e2b77e3
koenigj <koenigj@cip.ifi.lmu.de>
2022-03-15 15:51:30 +0100

    Rename II

--- src/gobi_cath_classification/rnn/pipeline.py
+++ src/gobi_cath_classification/rnn/pipeline.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import h5py
 from pathlib import Path
 from gobi_cath_classification.pipeline.data.data_loading import read_in_embeddings
 import numpy as np
@@ -8,7 +9,8 @@ from torch.nn.functional import one_hot
 REPO_ROOT_DIR = Path(__file__).parent.parent.parent.parent.absolute()
 DATA_DIR = REPO_ROOT_DIR / "data"
 
-emb = read_in_embeddings(DATA_DIR / "t5_xl_v3_half_cath_S100_RESIDUE.h5")
-print(f"Länge: {len(emb)}")
-print(emb.keys()[0])
-print(emb.values()[0].shape)
+h5_file = h5py.File(DATA_DIR / "t5_xl_v3_half_cath_S100_RESIDUE.h5")
+keys = list(h5_file.keys())
+keys = [keys[i:i + 10] for i in range(0, len(keys), 10)]
+print(keys)
+print(type(keys[0]))
