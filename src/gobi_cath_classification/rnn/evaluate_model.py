e087e2a3649fbe7112e459fc9b598238e9ef537b
koenigj <koenigj@cip.ifi.lmu.de>
2022-04-05 16:49:12 +0200

    Include temporal holdout II

--- src/gobi_cath_classification/rnn/evaluate_model.py
+++ src/gobi_cath_classification/rnn/evaluate_model.py
@@ -7,24 +7,14 @@ from typing import List, Optional, Dict
 
 import numpy as np
 import torch
-from gobi_cath_classification.pipeline.sample_weights import (
-    compute_inverse_sample_weights,
-    compute_inverse_class_weights,
-)
 
-from gobi_cath_classification.pipeline.utils import torch_utils
-from gobi_cath_classification.rnn.models import RNNModel, BRNN, BRNN_embedded, RNN_embedded
-from gobi_cath_classification.rnn.pipeline import load_data
 from gobi_cath_classification.pipeline.Evaluation import Evaluation
-from gobi_cath_classification.pipeline.utils.torch_utils import set_random_seeds
-from gobi_cath_classification.rnn.models import RNNModel, BRNN, one_hot_encode
 from gobi_cath_classification.rnn.pipeline import load_data
 from gobi_cath_classification.pipeline.data.data_loading import DATA_DIR
-from gobi_cath_classification.pipeline.data.Dataset import Dataset
 from gobi_cath_classification.pipeline import prediction
 
 _, _, train_labels, _, _, X_test, y_test, X_tmp, y_tmp = load_data(
-    DATA_DIR,
+    data_dir=DATA_DIR,
     without_duplicates=True,
     load_tmp_holdout_set=True
 )
