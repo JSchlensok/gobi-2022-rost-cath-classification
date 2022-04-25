d98ed1b9f8809882bb5717a4ff64cf8467f1508d
Julius Schlensok <schlensok@cip.ifi.lmu.de>
2022-04-25 14:05:56 +0200

    Adapt test_Dataset for lookup set inclusion and Dataset refactoring

--- src/gobi_cath_classification/pipeline/data/test_Dataset.py
+++ src/gobi_cath_classification/pipeline/data/test_Dataset.py
@@ -19,7 +19,6 @@ id1, id2, id3, id4, id5, id6 = [
 data1 = Dataset(
     X_train=x1,
     y_train=[CATHLabel(label) for label in [id1, id2, id3]],
-    train_labels=[CATHLabel(label) for label in [id1, id2, id3]],
     X_val=x2,
     y_val=[CATHLabel(label) for label in [id1, id2, id4]],
     X_test=x1,
@@ -27,12 +26,12 @@ data1 = Dataset(
     X_tmp_holdout=None,
     y_tmp_holdout=None,
     X_lookup=None,
-    y_lookup=None
+    y_lookup=None,
 )
 
 
 data1_with_strings = deepcopy(data1)
-data1_with_strings.load_strings(["1", "2", "5"], ["1", "2", "6"], ["1", "2", "5"], [])
+data1_with_strings.load_strings(["1", "2", "5"], ["1", "2", "6"], ["1", "2", "5"], [], [])
 
 
 def test_string_representation():
@@ -58,7 +57,6 @@ class TestFilteringForHLevel:
 data2 = Dataset(
     X_train=np.array([[1], [2]]),
     y_train=[CATHLabel("1.8.20.300"), CATHLabel("2.20.2.2")],
-    train_labels=[CATHLabel("1.8.20.300"), CATHLabel("2.20.2.2")],
     X_val=np.array([[3], [4]]),
     y_val=[CATHLabel("1.8.20.300"), CATHLabel("2.2.2.2")],
     X_test=np.array([[5], [6]]),
@@ -66,7 +64,7 @@ data2 = Dataset(
     X_tmp_holdout=None,
     y_tmp_holdout=None,
     X_lookup=None,
-    y_lookup=None
+    y_lookup=None,
 )
 
 data2 = data2.get_filtered_version("A")
